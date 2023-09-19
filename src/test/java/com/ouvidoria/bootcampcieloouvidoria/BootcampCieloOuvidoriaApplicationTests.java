069860eb2491e4765ba1672085fccd9ed6f5445a
lufemas <maschietto.jr@gmail.com>
2023-09-18 21:45:55 -0300

    first

---
+++ src/test/java/com/ouvidoria/bootcampcieloouvidoria/BootcampCieloOuvidoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouvidoria.bootcampcieloouvidoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampCieloOuvidoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
